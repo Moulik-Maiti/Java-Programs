fa6c0723c2d04610fa141cbb4bddea450e1726d0
Moulik Maiti <262538595+Moulik-Maiti@users.noreply.github.com>
2026-04-30 16:07:40 +0530

    Create Summer.java

---
+++ Summer.java
@@ -0,0 +1,26 @@
+interface Mango {
+    void taste();
+}
+
+class Winter implements Mango {
+    public void taste() {
+        System.out.println("Winter Mango is Sweet.");
+    }
+}
+
+class Summer implements Mango {
+    public void taste() {
+        System.out.println("Summer Mango is Juicy.");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Winter w = new Winter();
+        Summer s = new Summer();
+
+        w.taste();
+        s.taste();
+    }
+}
+}
